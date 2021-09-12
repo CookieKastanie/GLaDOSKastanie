83b6f4b7416ef0a131a5860d3ab95e3afb6eef6f
CookieKastanie <jerem0205@yahoo.fr>
2021-09-12 23:58:07 +0200

    ...

--- api_bot/modules/PFC.js
+++ api_bot/modules/PFC.js
@@ -5,33 +5,33 @@ const PFCEgalite = ["Hein ! tu lis dans mes circuits ?", "Wow égalité", "Arrê
 
 
 exports.jouer = (t) => {
-  let coup = -1;
-  if (t == "pierre") coup = 0;
-  if (t == "feuille") coup = 1;
-  if (t == "ciseaux") coup = 2;
+    let coup = -1;
+    if (t == "pierre") coup = 0;
+    if (t == "feuille") coup = 1;
+    if (t == "ciseaux") coup = 2;
 
-  if (coup != -1) {
-    let victoire = 2;
-    let coupBot = Math.floor(Math.random()*3);
+    if (coup != -1) {
+        let victoire = 2;
+        let coupBot = Math.floor(Math.random()*3);
 
-    if (coup == 0 && coupBot == 1) victoire = 0;
-    if (coup == 1 && coupBot == 2) victoire = 0;
-    if (coup == 2 && coupBot == 0) victoire = 0;
+        if (coup == 0 && coupBot == 1) victoire = 0;
+        if (coup == 1 && coupBot == 2) victoire = 0;
+        if (coup == 2 && coupBot == 0) victoire = 0;
 
-    if (coup == 0 && coupBot == 2) victoire = 1;
-    if (coup == 1 && coupBot == 0) victoire = 1;
-    if (coup == 2 && coupBot == 1) victoire = 1;
+        if (coup == 0 && coupBot == 2) victoire = 1;
+        if (coup == 1 && coupBot == 0) victoire = 1;
+        if (coup == 2 && coupBot == 1) victoire = 1;
 
-    let pfcMessage;
-    const rng = Math.floor(Math.random()*3);
-    switch (victoire) {
-      case 0: pfcMessage = PFCDefaite[rng]; break;
-      case 1: pfcMessage = PFCVictoire[rng]; break;
-      default: pfcMessage = PFCEgalite[rng];
+        let pfcMessage;
+        const rng = Math.floor(Math.random()*3);
+        switch (victoire) {
+            case 0: pfcMessage = PFCDefaite[rng]; break;
+            case 1: pfcMessage = PFCVictoire[rng]; break;
+            default: pfcMessage = PFCEgalite[rng];
+        }
+
+        return PFC[coupBot] +" "+ pfcMessage;
     }
 
-    return PFC[coupBot] +" "+ pfcMessage;
-  }
-
-  return null;
+    return null;
 }
